d4baf684d922aef50854795d753a57ae2acd04af
mwjaco <jacobs5977@gmail.com>
2023-02-02 22:03:01 -0600

    - add counter component - add tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count + 1);
+    }, 1000)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The current count is:</h1>
+      <h2>{count}</h2>
     </div>
   );
 }
